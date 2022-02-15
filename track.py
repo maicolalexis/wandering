a6b0f245532e851f6850a90e151185404670195f
unknown <SENA@DOSAPRCDIFSD167.FormacionSena.RED>
2022-02-15 14:59:17 -0500

    creamos las clases de wandering y track para el proyecto

---
+++ track.py
@@ -0,0 +1,13 @@
+class track:
+    def  __init__(self,x,y):
+        self.x = x
+        self.y = y
+        def move(self, delta_x, delta_y):
+            return track(self.x + delta_x, self.y + delta_y)
+
+
+        def distance(self, other_track):
+            delta_x = self.x - other_track.x
+            delta_y = self.y - other_track.y
+
+            return(delta_x**2 + delta_y**2)**0.5
---
+++ wandering.py
@@ -0,0 +1,16 @@
+from calendar import c
+import random;
+
+
+class wardering:
+    def __init__(self, name):
+
+        self,name = name
+
+class Comunwandering(borracho):
+    def __init__(self, name)
+    super().__init__(name)
+
+
+    def walk(self):
+        return random.choice([(0,1), (0,-1), (1,0), (-1,0)])
