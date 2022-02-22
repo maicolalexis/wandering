7008cf84a80241afc7f9baa0177d1a8c642cb3a2
maicolgaviria <maicolalexis222@gmail.com>
2022-02-22 15:36:15 -0500

    creamos la funcion main y el enter point

--- randomtrack.py
+++ randomtrack.py
@@ -1,4 +1,4 @@
-from turtle import distance, title
+
 from wandering import Comunwandering
 from track import Track
 from location import Location
@@ -11,24 +11,43 @@ def walking(location, wandering, step):
         location.move_wandering(wandering)
 
 
-        return beginning.distance(location.get_location(wandering))
+    return beginning.distances(location.get_location(wandering))
     
 
-def simulate_walk(steps, number, attempts, type_wandering):
+def simulate_walk(steps, number_attempts, type_wandering):
     wandering = type_wandering(name='Alirio')
     origen = Location(0, 0)
     distances = []
 
-        for _ in range(number_attempts):
-            track = Track()
-            track.add_wandering(wandering, origen)
-            simulate_walk = walking(track, wandering, steps)
-            distances.append(round(simulations_walk, 1))
-            return distances
+    for _ in range(number_attempts):
+        track = Track()
+        track.add_wandering(wandering, origen)
+        simulate_walk = walking(track, wandering, steps)
+        distances.append(round(simulations_walk, 1))
+    return distances
 
-        def graph(x, y):
-            graphics = figure(title='camino del errante,' x_axis_label='pasos', y_axis_label='distancia')
-            graphics.line(x, y, legend='distancia')
-            show(graphics)
+def graph(x, y):
+    graphics = figure(title='camino del errante,' x_axis_label='pasos', y_axis_label='distancia')
+    graphics.line(x, y, legend='distancia')
+    show(graphics)
 
 
+def main(distances_walk, number_attempts, type_wandering):
+    average_walking_distance = []
+    
+    for steps in distances_walk:
+        distances = simulate_walk(steps, number_attempts, type_wandering)
+        middle_distance = round(sum(distances) / len(distances), 4)
+        max_distances = max(distances)
+        min_distances = min(distances)
+        average_walking_distance.append(middle_distance)
+        print(f'{type_wandering.__name__} caminata aleatoria de {steps} pasos')
+        print(f'media=  {middle_distance}')
+        print(f'max = {max_distances}')
+        print(f'min = {min_distances}')
+    graph(distances_walk, average_walking_distance)
+
+if __name__ == '__main__':
+    distances_walk = [10,100,1000,10000]
+    number_attemps = 100
+    main(distances_walk, number_attempts, Comunwandering)
