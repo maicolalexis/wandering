6ee12e7dfbe4ff5e5cb48bd9d2fb406b8db55395
maicolgaviria <maicolalexis222@gmail.com>
2022-03-08 15:39:56 -0500

    Add 2

--- randomtrack.py
+++ randomtrack.py
@@ -3,8 +3,6 @@ from track import Track
 from location import Location
 from bokeh.plotting import figure, output_file,show
 
-def 
-
 from wandering import Comunwandering
 from track import Track
 from location import Location
